c5f9bc57c7dad51f94b87f3f90573e658171f223
Jason Kent <jasontkent@gmail.com>
2020-01-08 13:35:03 -0500

    indicate when measurement layer is unavailable

--- web/js/modules/layers/util.js
+++ web/js/modules/layers/util.js
@@ -21,6 +21,23 @@ import isBefore from 'date-fns/is_before';
 import isEqual from 'date-fns/is_equal';
 import isFirstDayOfMonth from 'date-fns/is_first_day_of_month';
 
+/**
+  *
+  * @param {*} def - layer definition
+  * @param {*} date - current selected app date
+  * @returns {Boolean} - True if layer is available at date, otherwise false
+  */
+export function availableAtDate(def, date) {
+  const availableDates = datesinDateRanges(def, date);
+  if (def.endDate && def.inactive) {
+    return date < new Date(def.endDate) && date > new Date(def.startDate);
+  }
+  if (!availableDates.length && !def.endDate && !def.inactive) {
+    return date > new Date(def.startDate);
+  }
+  return availableDates.length > 0;
+};
+
 export function getOrbitTrackTitle(def) {
   if (def.daynight && def.track) {
     return lodashStartCase(def.track) + '/' + lodashStartCase(def.daynight);
